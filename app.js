09651ae45dba770d7c35593b4bb4e5bf5a559b8e
Bilal Ashraf <bilalashraf6233@gmail.com>
2022-07-25 12:31:38 -0700

    Init setup node app

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('This is first commit')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+
+
+app.get('/', (req, res)=>{
+    res.json({
+        name: 'bilal',
+        email: 'bilalashraf@gmail.com'
+    })
+})
+
+app.listen(8000, ()=>{
+    console.log('Server is runing on port 8000')
+})
